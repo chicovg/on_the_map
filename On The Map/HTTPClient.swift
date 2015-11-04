9d2e3eb546e2e198872af29dde8191f898a4c8fd
Victor Guthrie <vguthrie@mbp-vguthrie.local>
2015-11-03 19:34:48 -0500

    adding additional client methods

--- On The Map/HTTPClient.swift
+++ On The Map/HTTPClient.swift
@@ -10,50 +10,80 @@ import Foundation
 
 class HTTPClient {
     
-    /*
-        pass in:
-            url
-            args
-            callback
-    
-    let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
-    request.HTTPMethod = "POST"
-    request.addValue("application/json", forHTTPHeaderField: "Accept")
-    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.HTTPBody = "{\"udacity\": {\"username\": \"account@domain.com\", \"password\": \"********\"}}".dataUsingEncoding(NSUTF8StringEncoding)
-    let session = NSURLSession.sharedSession()
-    let task = session.dataTaskWithRequest(request) { data, response, error in
-    if error != nil { // Handle error…
-    return
+    /**
+        Executes a GET request with the specified url and headers
+        The completion handler is executed after the request returns
+    */
+    func get(url: String, httpHeaders: [String: String]?, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
+        sendRequest(buildRequest(url, method: "GET", httpHeaders: httpHeaders, httpBody: nil), completionHandler: completionHandler)
     }
-    let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
-    println(NSString(data: newData, encoding: NSUTF8StringEncoding))
-    }
-    task.resume()
-
-     */
     
     /**
         Executes a POST request with the specified url, headers, and request body
         The completion handler is executed after the request returns
     */
-    func sendRequest(request: NSURLRequest, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
+    func post(url: String, httpHeaders: [String: String]?, httpBody: String, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
+        sendRequest(buildRequest(url, method: "POST", httpHeaders: httpHeaders, httpBody: httpBody), completionHandler: completionHandler)
+    }
+    
+    /**
+        Executes a PUT request with the specified url, headers, and request body
+        The completion handler is executed after the request returns
+    */
+    func put(url: String, httpHeaders: [String: String]?, httpBody: String, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
+        sendRequest(buildRequest(url, method: "PUT", httpHeaders: httpHeaders, httpBody: httpBody), completionHandler: completionHandler)
+    }
+    
+    /**
+        Executes a DELETE request with the specified url, and headers
+        The completion handler is executed after the request returns
+    */
+    func delete(url: String, httpHeaders: [String: String]?, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
+        sendRequest(buildRequest(url, method: "DELETE", httpHeaders: httpHeaders, httpBody: nil), completionHandler: completionHandler)
+    }
+    
+    /**
+        Checks the error object and response object to see if the request was successful
+    */
+    func responseSuccessful(response: NSURLResponse?, error: NSError?) -> Bool {
+        guard (error == nil) else {
+            print("There was an error with your request: \(error)")
+            return false
+        }
+        
+        guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+            if let response = response as? NSHTTPURLResponse {
+                print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+            } else if let response = response {
+                print("Your request returned an invalid response! Response: \(response)!")
+            } else {
+                print("Your request returned an invalid response!")
+            }
+            return false
+        }
+        return true
+    }
+    
+    private func sendRequest(request: NSURLRequest, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
         let session = NSURLSession.sharedSession()
         let task = session.dataTaskWithRequest(request, completionHandler: completionHandler)
         task.resume()
     }
     
-    func post(url: String, httpHeaders: [String: String], httpBody: String, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
-        let u = NSURL(string: url)
-        let request = NSMutableURLRequest(URL: u!)
-        request.HTTPMethod = "POST"
+    private func buildRequest(url: String, method: String, httpHeaders: [String: String]?, httpBody: String?) -> NSURLRequest {
+        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
+        request.HTTPMethod = method
         
-        for (key, value) in httpHeaders {
-            request.addValue(value, forHTTPHeaderField: key)
+        if let headers = httpHeaders {
+            for (key, value) in headers {
+                request.addValue(value, forHTTPHeaderField: key)
+            }
         }
-        request.HTTPBody = httpBody.dataUsingEncoding(NSUTF8StringEncoding)
-        sendRequest(request, completionHandler: completionHandler)
+        
+        if let body = httpBody {
+            request.HTTPBody = body.dataUsingEncoding(NSUTF8StringEncoding)
+        }
+        return request
     }
-    
     
 }
--- On The Map/UdacityClient.swift
+++ On The Map/UdacityClient.swift
@@ -14,6 +14,10 @@ class UdacityClient : HTTPClient {
     
     let kUdacityBaseUrl = "https://www.udacity.com/api"
     let kSessionPath = "/session"
+    let kUsersPath = "/users"
+    
+    let kXsrfCookieName = "XSRF-TOKEN"
+    let kXsrfTokenKey = "X-XSRF-TOKEN"
     
     /** Uses the Udacity session API to create a new session
         If the request is successful ..
@@ -27,36 +31,91 @@ class UdacityClient : HTTPClient {
         post(url, httpHeaders: httpHeaders, httpBody: httpBody, completionHandler: {
             data, response, error in
             
-            guard (error == nil) else {
-                print("There was an error with your request: \(error)")
-                failureHandler()
-                return
-            }
-            
-            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
-                if let response = response as? NSHTTPURLResponse {
-                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
-                } else if let response = response {
-                    print("Your request returned an invalid response! Response: \(response)!")
-                } else {
-                    print("Your request returned an invalid response!")
-                }
-                failureHandler()
-                return
-            }
-            
-            if let data = data {
-                /*  success:
+            if self.responseSuccessful(response, error: error) {
+                if let data = data {
+                    /*  success:
                     Optional({"account": {"registered": true, "key": "628108572"}, "session": {"id": "1478090449S3f8b6354fb908708e7d0677bbdf1482e", "expiration": "2016-01-02T12:40:49.018640Z"}}) */
-                /* failure:
+                    /* failure:
                     Optional({"status": 403, "error": "Account not found or invalid credentials."}) */
-                
-                let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
-                print(NSString(data: newData, encoding: NSUTF8StringEncoding))
-                // TODO do I store the session id ??
-                
-                successHandler()
+                    
+                    let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
+                    print(NSString(data: newData, encoding: NSUTF8StringEncoding))
+                    // TODO do I store the session id ??
+                    
+                    successHandler()
+                    return
+                }
             }
+            failureHandler()
         })
     }
+    
+    /** Uses the Udacity session API to delete the current session info (i.e. log out)
+        If the request is successful ..
+        If the request fails, the specified failure handler will be executed
+    */
+    func deleteSession(successHandler: () -> Void, failureHandler: () -> Void) {
+        let url = kUdacityBaseUrl + kSessionPath
+        var httpHeaders: [String: String] = [:]
+        
+        let sharedCookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+        let xsrfCookie = sharedCookieStorage.cookies!.filter({(cookie) in cookie.name == kXsrfCookieName}).first
+        if let cookie = xsrfCookie {
+            httpHeaders[kXsrfTokenKey] = cookie.value
+        }
+        delete(url, httpHeaders: httpHeaders, completionHandler: {
+            data, response, error in
+
+            if self.responseSuccessful(response, error: error) {
+                if let data = data {
+                    
+                    let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
+                    print(NSString(data: newData, encoding: NSUTF8StringEncoding))
+                    // TODO do I store the session id ??
+                    
+                    successHandler()
+                    return
+                }
+            }
+            failureHandler()
+        })
+    }
+    /*
+    let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/users/3903878747")!)
+    let session = NSURLSession.sharedSession()
+    let task = session.dataTaskWithRequest(request) { data, response, error in
+    if error != nil { // Handle error...
+    return
+    }
+    let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
+    println(NSString(data: newData, encoding: NSUTF8StringEncoding))
+    }
+    */
+    func getUserData(userId: String?, successHandler: () -> Void, failureHandler: () -> Void) {
+        if let userId = userId {
+            let url = kUdacityBaseUrl + kUsersPath + userId
+            
+            get(url, httpHeaders: nil, completionHandler: {
+                data, response, error in
+                
+                if self.responseSuccessful(response, error: error) {
+                    if let data = data {
+                        
+                        let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
+                        print(NSString(data: newData, encoding: NSUTF8StringEncoding))
+                        // TODO do I store the session id ??
+                        
+                        successHandler()
+                        return
+                    }
+                }
+                failureHandler()
+            })
+        } else {
+            print("Cannot get user data because user id was not provided")
+        }
+    }
+    
+    
+    
 }
